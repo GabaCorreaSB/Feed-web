0df132c33729c9628de337b1822f095691bd43e4
Gabriel Correa <gabrielcorreasb@protonmail.com>
2023-05-20 04:11:01 -0300

    Add libfinance here

--- lib/libyfinance.py
+++ lib/libyfinance.py
@@ -8,9 +8,9 @@ import pandas as pd
 class Ticker():
 	def __init__(self, ticker, period, interval):
 		self._ticker = ticker
-        self._period = period
-        self._interval = interval
- 
-    def gen_ticks(self):
-        ticker_data = yf.download(tickers=self._ticker, period=self._period, interval=self._interval)
-        ticker_data.to_csv('../etc/ticker_data.csv')
+		self._period = period
+		self._interval = interval
+
+	def gen_ticks(self):
+		ticker_data = yf.download(tickers=self._ticker, period=self._period, interval=self._interval)
+		ticker_data.to_csv('../etc/ticker_data.csv')
