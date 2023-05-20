81c4bbfc53346847ed2e1c81842a609212733219
Gabriel Correa <gabrielcorreasb@protonmail.com>
2023-05-20 03:50:07 -0300

    Add a lib to actually get real ticker data from yfinance (building an infra to run any ticker info)

---
+++ lib/libyfinance.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2023 Gabriel Correa <gabriel.correasb@protonmail.com>
+#
+
+import yfinance as yf
+import pandas as pd
+
+class Ticker():
+	def __init__(self, ticker, period, interval):
+		self._ticker = ticker
+        self._period = period
+        self._interval = interval
+ 
+    def gen_ticks(self):
+        ticker_data = yf.download(tickers=self._ticker, period=self._period, interval=self._interval)
+        ticker_data.to_csv('../etc/ticker_data.csv')
